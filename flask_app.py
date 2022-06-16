5cbfa794eeeeae0765f5709dbcfb4fb5fcc47130
Rebecca Martinez <you@example.com>
2022-06-16 15:41:12 +1000

    flask

---
+++ flask_app.py
@@ -0,0 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from rhino3dm import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
